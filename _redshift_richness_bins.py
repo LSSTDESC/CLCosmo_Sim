dac6dcea7238e91d81622004e3f98bad6cb84be2
payerne <constantin.payerne@etu.univ-grenoble-alpes.fr>
2024-07-15 22:53:38 +0200

    small changes

--- _redshift_richness_bins.py
+++ _redshift_richness_bins.py
@@ -3,8 +3,10 @@ import numpy as np
 def binning(corner): return [[corner[i],corner[i+1]] for i in range(len(corner)-1)]
 
 z_corner = np.array([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1])
+#z_corner = np.array([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 1])
 #z_corner = np.array([0.2, 0.4, 0.6, 0.8,  1])
 Z_bin = binning(z_corner)
 rich_corner = np.array([20, 35, 70, 100, 200])
+#rich_corner = np.array([20, 30, 50, 80, 120, 200])
 rich_corner = np.array([r for r in rich_corner])
 Obs_bin = binning(rich_corner)
